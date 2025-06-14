a400a8116c572dd04d5b3c245b721aea8d07d5f4
Daniil <soft_cat467@mail.ru>
2025-06-14 20:27:07 +0300

    feat: add readline-sync dependency and implement user greeting in CLI
    - Updated package.json to include readline-sync as a dependency.
    - Created eslint.config.js for ESLint configuration with recommended settings.
    - Implemented greetUser function in cli.js to welcome users and capture their name.

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+export const greetUser = () => {
+ console.log("Welcome to the Brain Games!")
+ let name = readlineSync.question('Your answer: ');
+ console.log('Hi, ' + name + '!');
+}
+
+
+
+
